8b1d4cc171cbfc8d4da5279010eee32d7de49341
Héricles Emanuel <hericles.me@gmail.com>
2019-10-27 20:53:28 -0300

    setup config

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+import configparser
+
+config = configparser.ConfigParser()
+
+config_path = os.path.dirname(os.path.abspath(__file__)) + '/setup.cfg'
+
+config.read(config_path)
+
+host = config.get('general', 'host')
+
+port = config.get('general', 'port')
+
+debug = config.get('general', 'debug').lower()
+
+if debug == "true":
+    debug = True
+elif debug == "false":
+    debug = False
