2b72c320b9ff560a05a5dea80b4bb71fb1294946
Héricles Emanuel <hericles.me@gmail.com>
2019-10-27 20:54:39 -0300

    update api to use config values

--- podcast/api/api.py
+++ podcast/api/api.py
@@ -1,15 +1,19 @@
 from flask import Flask, request
+import config
 import json
 
 app = Flask(__name__)
 
-def main():
-    app.run(host="0.0.0.0", port=3000, debug=True)
+
+def start():
+    app.run(host=config.host, port=config.port,
+            debug=config.debug)
 
 
 @app.route("/", methods=["GET"])
 def index():
     return "Hello, Podcast!"
+
 
 @app.route("/create", methods=["POST"])
 def create_container():
@@ -17,5 +21,6 @@ def create_container():
         print(json.loads(request.data))
         return "Create, World!"
 
+
 if __name__ == "__main__":
-    main()
+    start()
