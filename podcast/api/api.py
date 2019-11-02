579f55751a0ef5745a578716c1b589550e722204
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-11-02 11:10:07 -0300

    Add Kubernetes provisioning (#6)

--- podcast/api/api.py
+++ podcast/api/api.py
@@ -1,25 +1,31 @@
 from flask import Flask, request
+from flask_restful import Resource, Api
+
 import config
-import json
+from podcast.controller import controller
+from podcast.utils import random_string
+
+class CastCRUD(Resource):
+
+    def post(self):
+        json = request.get_json()
+        image = json.get('image')
+        job_id = random_string(string_length=5)
+
+        if image is not None:
+            return controller.create_job(job_id, image=image)
+        else:
+            return controller.create_job(job_id)
+
 
 app = Flask(__name__)
+api = Api(app)
 
+api.add_resource(CastCRUD, "/create/")
 
 def start():
     app.run(host=config.host, port=config.port,
             debug=config.debug)
-
-
-@app.route("/", methods=["GET"])
-def index():
-    return "Hello, Podcast!"
-
-
-@app.route("/create", methods=["POST"])
-def create_container():
-    if request.method == "POST":
-        print(json.loads(request.data))
-        return "Create, World!"
 
 
 if __name__ == "__main__":
